d2854fb9085211f5a6b3922fabe3341176722a60
Sreejita Bhardwaj <sreejitabhardwaj2005@gmail.com>
2024-09-18 09:59:59 +0530

    Add files via upload

---
+++ stack_implementation.cpp
@@ -0,0 +1,84 @@
+// Sreejita Bhardwaj
+// Entc
+// Exp 17: stack implementation using array
+
+#include <iostream>
+using namespace std;
+
+const int n = 100;
+int stack[n], top = -1;
+
+void push(int val) {
+    if (top >= n - 1) {
+        cout << "Stack overflow" << endl;
+    } else {
+        stack[++top] = val;
+    }
+}
+
+void pop() {
+    if (top < 0) {
+        cout << "Stack underflow" << endl;
+    } else {
+        cout << "The popped element is: " << stack[top--] << endl;
+    }
+}
+
+void display() {
+    if (top >= 0) {
+        cout << "Stack elements are: ";
+        for (int i = top; i >= 0; i--) {
+            cout << stack[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Stack is empty" << endl;
+    }
+}
+
+int main()
+{
+    int ch,val;
+    cout<<"1) Push in stack"<<endl;
+    cout<<"2) Pop from stack"<<endl;
+    cout<<"3) display stack"<<endl;
+    cout<<"4) exit"<<endl;
+
+    do
+    {
+        cout<<"enter choice: "<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+            {
+                cout<<"enter the value that has to be pushed"<<endl;
+                cin>>val;
+                push(val);
+                break;
+            }
+
+            case 2:
+            {
+                pop();
+                break;
+            }
+            case 3:
+            {
+                display();
+                break;
+            }
+            case 4:
+            {
+                cout<<"exit"<<endl;
+                break;
+            }
+            default:
+            {
+                cout<<"Invalid choice"<<endl;
+            }
+        }
+    }
+    while (ch!=4);
+    return 0;
+}
